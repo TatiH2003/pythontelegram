d9b84a41c74eb55784d89f299b22d64678b1f061
harshith <harshithtatiparthi@gmail.com>
2022-06-17 15:00:15 +0530

    scores removed

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from telegram.ext import *
 import response as r
 
 import os
+
 TOKEN = "5412632814:AAGwPC39OmMf0CfY6MjluroUO6dOv_xhI7c"
-user_wins = 0
-comp_wins = 0
 
 
 def start_command(update, context):
     yourname = update.message.from_user.first_name
-    msg = "Hi "+yourname+"! Welcome to Rock app bot"
+    msg = "Hi " + yourname + "! Welcome to Rock app bot"
     context.bot.send_message(update.message.chat.id, msg)
     msg_2 = "Select your choice (rock/Paper/scissors)."
     context.bot.send_message(update.message.chat.id, msg_2)
@@ -23,42 +22,12 @@ def handle_message(update, context):
     text = str(update.message.text).lower()
     response = r.rock_paper(text)
     update.message.reply_text(response)
-
-    input_ = str(response).lower()
-    global user_wins, comp_wins
-
-    if "you won" in input_:
-        user_wins += 1
-        update.message.reply_text(f'User score : {user_wins}.')
-
-    elif "you lost" in input_:
-        comp_wins += 1
-        update.message.reply_text(f'Computer score : {comp_wins}')
-    else:
-        update.message.reply_text(
-            f'User score : {user_wins}.\n'
-            f'computer score : {comp_wins}.\n'
-        )
-
     update.message.reply_text("Select your choice (rock/Paper/scissors).")
     print(response)
 
+
 def quit_command(update, context):
-    global user_wins, comp_wins
-    if user_wins > comp_wins:
-        update.message.reply_text("Hurry!!! You won the match.")
-        user_wins=0
-        comp_wins=0
-
-    elif user_wins == comp_wins:
-        update.message.reply_text("It`s a tie.. Good luck Next time.")
-        user_wins = 0
-        comp_wins = 0
-
-    else:
-        update.message.reply_text("You lost the match..Good luck next time.")
-        user_wins = 0
-        comp_wins = 0
+    update.message.reply_text("Bye!! See you next time")
 
 
 def error(update, context):
@@ -74,9 +43,9 @@ def main():
     dp.add_handler(CommandHandler("quit", quit_command))
     dp.add_handler(MessageHandler(Filters.text, handle_message))
 
-    updater.start_webhook(listen="0.0.0.0",port=os.environ.get("PORT", 443),
+    updater.start_webhook(listen="0.0.0.0", port=os.environ.get("PORT", 443),
                           url_path=TOKEN,
-                          webhook_url="https://rock-paper-01.herokuapp.com/"+TOKEN)
+                          webhook_url="https://rock-paper-01.herokuapp.com/" + TOKEN)
     updater.idle()
 
 
